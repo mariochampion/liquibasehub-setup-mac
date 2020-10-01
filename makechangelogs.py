9e4a6c6b84d4526a1dd0db1316350f4040e8f815
mariochampion <mario@mariochampion.com>
2020-10-01 09:40:08 -0500

    bump to v.11 to add STARTH2 switch

--- makechangelogs.py
+++ makechangelogs.py
@@ -26,7 +26,7 @@ import os, sys, subprocess, time
 ######### OVERALLCONFIGS
 ### TOOL VARS
 toolname = "python_gen_tool"
-toolversion = str("_v.10")
+toolversion = str("_v.11")
 
 ## TODO: MAKE THESE INTERACTIVE INPUTS
 #num_of_files = int(5)
